8e30b7d8c40c5f9c6ac71b9c4fd80a80a7a3c1dd
jihun_oh <ohji100629@naver.com>
2022-03-12 13:45:49 +0900

    complete easter egg test

--- src/test/java/config/easteregg/EasterEggConfigTest.java
+++ src/test/java/config/easteregg/EasterEggConfigTest.java
@@ -1,38 +1,51 @@
 package config.easteregg;
 
-import config.easteregg.property.EasterEggProperty;
-import config.easteregg.property.EasterEggTemplateProperty;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
-
-
+// TODO
+// 단순 get 을 테스트할 필요가 있을까?
+// 나중에 로직이 추가되어서 복작하게 get 을 가져오는 것을 고려하면 get 도 테스트 하는게 맞아보인다.
 class EasterEggConfigTest {
-    private static Stream<Arguments> provideType() {
-        return Arrays.stream(EasterEggType.values())
-                .map(EasterEggType::getValue)
-                .map(it -> Arguments.of(it));
+    private static EasterEggConfig createEasterEggConfigFromFile() {
+        return EasterEggConfig.create();
     }
 
-    private static List<EasterEggProperty> createEasterEggInfo(String type) throws Exception {
-        return List.of(EasterEggProperty.ofJackSon("/testURL", "testContent", type));
-    }
-
-    @DisplayName("EasterEggConfig 가 정상적으로 생성되어야 합니다.")
-    @ParameterizedTest
-    @MethodSource(value = "provideType")
-    void test1(String type) throws Exception {
+    @DisplayName("config 파일을 통해서 정상적으로 객체가 생성되어야 합니다.")
+    @Test
+    void test0() {
         //given
-        List<EasterEggProperty> easterEggProperties = createEasterEggInfo(type);
+        //when
+        Throwable actual = Assertions.catchThrowable(() -> createEasterEggConfigFromFile());
+
+        //then
+        Assertions.assertThat(actual)
+                .isNull();
+    }
+
+    @DisplayName("EasterEggProperty 를 정상적으로 가져옵니다.")
+    @Test
+    void test1() {
+        //given
+        EasterEggConfig easterEggConfig = createEasterEggConfigFromFile();
 
         //when
-        Throwable actual = Assertions.catchException(() -> EasterEggConfig.ofJackSon(easterEggProperties, List.of(new EasterEggTemplateProperty(type, "test format"))));
+        Throwable actual = Assertions.catchThrowable(() -> easterEggConfig.getEasterEggProperties());
+
+        //then
+        Assertions.assertThat(actual)
+                .isNull();
+    }
+
+    @DisplayName("EasterEggTemplateProperty 를 정상적으로 가져옵니다.")
+    @Test
+    void test2() {
+        //given
+        EasterEggConfig easterEggConfig = createEasterEggConfigFromFile();
+
+        //when
+        Throwable actual = Assertions.catchThrowable(() -> easterEggConfig.getEasterEggTemplateProperties());
 
         //then
         Assertions.assertThat(actual)
--- src/test/java/content/factory/EasterEggMessageFactoryTest.java
+++ src/test/java/content/factory/EasterEggMessageFactoryTest.java
@@ -1,9 +1,9 @@
 package content.factory;
 
+import config.easteregg.EasterEgg;
 import content.message.Message;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import repository.easteregg.EasterEggRepository;
@@ -11,12 +11,11 @@ import util.Util;
 
 import java.lang.reflect.Constructor;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 class EasterEggMessageFactoryTest {
 
-    private static EasterEggRepository createRepository(Map<Path, String> map) throws Exception {
+    private static EasterEggRepository createRepository(Map<Path, EasterEgg> map) throws Exception {
         Constructor<EasterEggRepository> constructor = EasterEggRepository.class.getDeclaredConstructor(Map.class);
         constructor.setAccessible(true);
 
@@ -41,28 +40,18 @@ class EasterEggMessageFactoryTest {
         Path factoryPath = Util.normalizePath(Path.of(_factoryPath));
         Path repositoryPath = Util.normalizePath(Path.of(_repositoryPath));
 
-        EasterEggRepository easterEggRepository = createRepository(Map.of(repositoryPath, "content"));
+        EasterEggRepository easterEggRepository = createRepository(Map.of(repositoryPath, new EasterEgg(repositoryPath, "content")));
         EasterEggMessageFactory easterEggMessageFactory = createFactory(factoryPath, easterEggRepository);
 
         return easterEggMessageFactory;
     }
 
-
-    @Test
-    void test2() throws Exception {
-        EasterEggRepository content = createRepository(Map.of(Paths.get("/temp"), "content"));
-        System.out.println("temp");
-    }
-
-    //    //TODO 1
-//    // 계속해서 test 를 하기 위해서 실제 데이터인 url 과 테스트 suit 가 동일한 값으로 셋팅되어야 한다.
     @DisplayName("path 가 존재하면 true 값이 출력되야 합니다.")
     @ParameterizedTest
     @ValueSource(strings = {"/test1", "/test2", "/test3"})
     void test1(String _path) throws Exception {
         //given
         EasterEggMessageFactory easterEggMessageFactory = createBy(_path);
-//                EasterEggMessageFactory easterEggMessageFactory = new EasterEggMessageFactory(path);
 
         //when
         boolean actual = easterEggMessageFactory.isSupport();
@@ -82,14 +71,14 @@ class EasterEggMessageFactoryTest {
         //when
         boolean actual = easterEggMessageFactory.isSupport();
 
+        //then
         Assertions.assertThat(actual)
                 .isFalse();
     }
 
-    //
     @DisplayName("easter egg 의 path 이면 message 를 생성해야 합니다.")
     @ParameterizedTest
-    @ValueSource(strings = {"/test1", "/test2", "/test3"})
+    @ValueSource(strings = {"/test", "/test2", "/test3"})
     void test3(String _path) throws Exception {
         //given
         EasterEggMessageFactory easterEggMessageFactory = createBy(_path);
@@ -102,7 +91,6 @@ class EasterEggMessageFactoryTest {
                 .isNotNull();
     }
 
-    //
     @DisplayName("easter egg 의 path 가 아니면 exception 이 발생합니다.")
     @ParameterizedTest
     @ValueSource(strings = {"/not_exist_dir"})
@@ -117,28 +105,4 @@ class EasterEggMessageFactoryTest {
         Assertions.assertThat(actual)
                 .isNotNull();
     }
-//
-
-//    // test 에서 처음
-//    // 1. method 에서 필요한 param 을 객체 외부에서 생성한다.
-//    @DisplayName("easter egg message factory proto")
-//    @Test
-//    void proto() {
-//        Path _target = Path.of("/target");
-//
-//        EasterEggMessageFactory easterEggMessageFactory = new EasterEggMessageFactory(_target) {
-//            Path target = _target;
-//
-//            @Override
-//            public Message createMessage() {
-//                return super.createMessage();
-//            }
-//
-//            @Override
-//            public boolean isSupport() {
-//                return super.isSupport();
-//            }
-//        };
-//    }
-
 }
